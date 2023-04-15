5007f19c766fa4fa740c0c21c08996f0232e2423
Idan Or <78268847+Nashdidan@users.noreply.github.com>
2023-04-15 17:56:42 +0300

    init

---
+++ ModernUI/ModernUI/Core/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace ModernUI.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ModernUI/ModernUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ModernUI.Core;
+using ModernUI.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernUI.MVVM.ViewModel
+{
+    public class MainViewModel: ObservableObject
+    {
+
+		public HomeViewModel Home { get; set; }
+		public DiscoveryViewModel Discovery { get; set; }
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set { _currentView = value; OnPropertyChanged("CurrentView"); }
+		}
+
+        public MainViewModel()
+        {
+            Home = new HomeViewModel();
+            Discovery = new DiscoveryViewModel();
+            CurrentView = Home;
+            HomeViewCommand = new RelayCommand((o) => CurrentView = Home);
+            DiscoveryViewCommand = new RelayCommand((o) => CurrentView = Discovery);
+        }
+
+        public RelayCommand HomeViewCommand { get; }
+        public RelayCommand DiscoveryViewCommand { get;}
+    }
+}
